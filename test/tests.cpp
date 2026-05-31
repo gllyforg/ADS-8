b735276421dc00c194f73ee470610b3d9b118ec7
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 21:51:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -16,23 +16,20 @@ TEST(lab8, test1_2) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int node = tree.search("pierre");
-    int count = (node != nullptr) ? node->count : 0;
+    int count = tree.search("pierre");
     EXPECT_EQ(count, 1963);
 }
 TEST(lab8, test1_3) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int node = tree.search("natasha");
-    int count = (node != nullptr) ? node->count : 0;
+    int count = tree.search("natasha");
     EXPECT_EQ(count, 1212);
 }
 TEST(lab8, test1_4) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int node = tree.search("andrew");
-    int count = (node != nullptr) ? node->count : 0;
+    int count = tree.search("andrew");
     EXPECT_EQ(count, 1143);
 }
