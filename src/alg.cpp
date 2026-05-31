1d67c8195638652713d89ab258adbf7e97a15087
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 21:54:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
 // Copyright 2021 NNTU-CS
 #include  "bst.h"
 
+#include <string>
+#include <algorithm>
+#include <iostream>
+
 void makeTree(BST<std::string>& tree, const char* filename) {
   // поместите сюда свой код
   std::ifstream file(filename);
